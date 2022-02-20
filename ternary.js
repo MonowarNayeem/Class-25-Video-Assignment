851f6789ab500d1f956cb9c95971737013c66453
monowar-lwd <a1monoar@gmail.com>
2022-02-21 02:01:56 +0600

    class 25 video assignment

---
+++ ternary.js
@@ -0,0 +1,16 @@
+var n = 11
+var str = ''
+
+if(n % 2 === 0){
+    str = 'Even'
+}
+else{
+    str = 'Odd'
+}
+console.log(str);
+
+// Ternary operator....
+// Condition ? true side : false side
+
+var check = n % 2 === 0? 'Even': 'Odd'
+console.log(check);
